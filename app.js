346926daedcc188cfd7035a2dfdbb99aa6f3b8b5
smartcentric <smartcentric@gmail.com>
2020-03-10 21:33:45 +0530

    pug files

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'pug'); // set is used to store the value or saying the express to use this value
+                  // folder name
+app.set('views', 'views');
+// app.get() used to get the value
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {docTitle: '404'});
 });
 
 app.listen(3000);
