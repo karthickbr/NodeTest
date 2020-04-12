5780d5f22bc1f8cc63d850b334fbee9852ac26fd
smartcentric <smartcentric@gmail.com>
2020-04-13 00:13:12 +0530

    upto session and cookie

--- app.js
+++ app.js
@@ -2,36 +2,76 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const mongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-
+const User = require('./models/user');
+const MONGODB_URI = 'mongodb+srv://NodeFull:node@cluster0-dbrlk.mongodb.net/shop';
 const app = express();
+
+const store = new mongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions',
+  // expires: 600000
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'my secret', 
+  resave: false, 
+  saveUninitialized: false, 
+  store: store, 
+  // cookie:{expires:60000, maxAge: 30000}
+}));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+
+  if(!req.session.user) {
+       return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes)
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(4000);
-});
+mongoose
+  .connect(MONGODB_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(4000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
---
+++ controllers/auth.js
@@ -0,0 +1,30 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById('5e91dbfd68e83606a44cf6b2')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      })
+     
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
